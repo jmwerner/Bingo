3fe9ff6e9c2dde6807a20dad81b006fdb11bfd4a
Jeremy Werner <jmwerner1123@me.com>
2017-05-16 22:41:08 -0700

    Add first draft of bingo card

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Bingo
-Repo for meeting bingo
+
+Repo for meeting bingo website
+
+Code base from this kind soul [here](https://github.com/jeffehobbs/HTML5-bingo/)
+
+
+
---
+++ js/data.js
@@ -0,0 +1,38 @@
+
+var headerText = "Meeting Bingo";
+
+var winText = "Winner";
+
+var JSONBingo = {"squares": [
+        {"square": "Live into"},
+        {"square": "Haircut"},
+        {"square": "Alignment"},
+        {"square": "Holistic"},
+        {"square": "KPI"},
+        {"square": "Deliverables"},
+        {"square": "Infrastructure"},
+        {"square": "Streamline"},
+        {"square": "Test and learn"},
+        {"square": "Synergy"},
+        {"square": "Mindset"},
+        {"square": "Enable"},
+        {"square": "Ecosystem"},
+        {"square": "Optimize"},
+        {"square": "Tactical"},
+        {"square": "Misaligned"},
+        {"square": "Framework"},
+        {"square": "Traction"},
+        {"square": "Lever"},
+        {"square": "Incentivize"},
+        {"square": "Deep dive"},
+        {"square": "Mission Critical"},
+        {"square": "Productionalize"},
+        {"square": "Benchmark"},    	
+        {"square": "Robust"},    	
+        {"square": "Leverage"},    	
+        {"square": "Ballpark"}, 
+        {"square": "Buy-in"},    	
+        {"square": "Granular"},    	
+        {"square": "Disruptive"}    	
+    ]
+};
